0f082a9581e1c2b8e0dcfd2b1b92454ecbda8c77
Nate Fikru <natefikru@gmail.com>
2015-10-05 20:52:04 -0500

    altered print statements, added to fork statements

--- shell.c
+++ shell.c
@@ -3,28 +3,35 @@
 #include<string.h>
 #include<unistd.h>
 
-int main()
-{
-	commandLoop();
 
-	return EXIT_SUCCESS;
+int lsh_exit(char **args);
+int lsh_cd(char **args) ;
+
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &lsh_exit,
+	&lsh_cd
+};
+
+int lsh_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
 }
 
-void commandLoop(void)
-{
-	char *line;
-	char **args;
-	int status;
-
-	do {
-		printf("Shell has started");
-		line = inputLine();
-		args = parseLine(line);
-		status = initiateArgs(args);
-
-		free(line);
-		free(args);
-	} while(status);
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
 }
 
 char *inputLine(void)
@@ -35,8 +42,10 @@ char *inputLine(void)
 	return line;
 }
 
+
+
 #define tokenSize 64
-#define tokenDivider = " \t\r\n\a"
+#define tokenDivider " \t\r\n\a"
 char **parseLine(char *line)
 {
 
@@ -69,24 +78,6 @@ char **parseLine(char *line)
 	return tokens;
 }
 
-//TODO
-
-int initiateArgs(char **args)
-{
-	int i;
-	if(args[0] == NULL){
-		return 1;
-	}
-	for (i = 0; i < lsh_num_builtins(); i++) {
-		if (strcmp(args[0], builtin_str[i]) == 0) 
-		{
-			return (*builtin_func[i])(args);
-    		}
-  	}
-
-  	return startCommands(args);
-}
-
 //tokens in an array
 int startCommands(char **args)
 {
@@ -95,27 +86,73 @@ int startCommands(char **args)
 
 	pid = fork();
 	if(pid == 0){
-		execvp(args[0], args);
+		printf("child has been created\n");
+		if(strcmp(args[0], "ls") == 0){
+			char *execArgs[] = {"ls", NULL};
+			//execvp(args[0], args);
+			execvp("ls", execArgs);
+		}
+		if(strcmp(args[0], "bash") == 0){
+			char *execArgs[] = {"bash","mkdir", NULL};
+			//execvp(args[0], args);
+			execvp("bash", execArgs);
+		}
 	}
 	else if(pid < 0){
 		fprintf(stderr, "Fork Failed");
 	}
 	else {
 		wait(NULL);
-		printf("Child has completed");
+		printf("Child has completed\n");
 	}
 
 	return 1;
 
 }
 
+int initiateArgs(char **args)
+{
+	int i;
+	if(args[0] == NULL){
+		return 1;
+	}
+	for (i = 0; i < lsh_num_builtins(); i++) {
+		if (strcmp(args[0], builtin_str[i]) == 0)
+		{
+			return (*builtin_func[i])(args);
+    		}
+  	}
+
+  	return startCommands(args);
+}
 
 
 
+void commandLoop(void)
+{
+	char *line;
+	char **args;
+	int status;
 
+	do {
+		printf("Shell has started\n");
+		line = inputLine();
+		args = parseLine(line);
+		status = initiateArgs(args);
 
+		free(line);
+		free(args);
+	} while(status);
+}
 
+int main()
+{
+	commandLoop();
+  printf("Exiting Shell");
+	return EXIT_SUCCESS;
+}
 
-
-
-
+int lsh_exit(char **args)
+{
+  return 0;
+}
